d4e6f72eb97ff8056e896a2198c2ec7fa1bd7781
0511uni <105358271+0511uni@users.noreply.github.com>
2022-08-01 21:16:10 +0900

    TaxCalクラス内移動

--- Calculator-1/CalculatorItemCreateFactory.cs
+++ Calculator-1/CalculatorItemCreateFactory.cs
@@ -6,45 +6,6 @@ namespace Calculator_1
 {
     internal class CalculatorItemCreateFactory
     {
-        internal static void TaxCalCreate(TextBox calBox, TextBox formulaBox, TextBox allCalBox, Button taxButton, Button noTaxButton, object sender)
-        {
-            if (sender.Equals(taxButton))
-            {
-                if (calBox.Text == "")
-                {
-                    return;
-                }
-
-                string a = calBox.Text;
-
-                string tax = a + "* 1.1";
-
-                DataTable dt = new DataTable();
-                var result = dt.Compute(tax, "");
-
-                calBox.Text = Math.Floor((decimal)result).ToString();
-                formulaBox.Text = $"{a}円 ⇒ 税込 : " + Math.Floor((decimal)result) + "円";
-                allCalBox.Text += formulaBox.Text + "\r\n";
-            }
-            else if (sender.Equals(noTaxButton))
-            {
-                if (calBox.Text == "")
-                {
-                    return;
-                }
-                string a = calBox.Text;
-
-                string tax = a + "/ 1.1 + 0.1";
-
-                DataTable dt = new DataTable();
-                var result = dt.Compute(tax, "");
-
-                calBox.Text = Math.Floor((decimal)result).ToString();
-                formulaBox.Text = $"{a}円 ⇒ 税抜 : " + Math.Floor((decimal)result) + "円";
-                allCalBox.Text += formulaBox.Text + "\r\n";
-            }
-        }
-
         internal static void DecimalPointButton(TextBox calBox, TextBox formulaBox, TextBox allCalBox, object sender)
         {
             if (formulaBox.Text.Contains("=") || formulaBox.Text.Contains("税"))
@@ -107,6 +68,45 @@ namespace Calculator_1
             ////var addButton = "=";
             //calBox.Text = a.ToString();//addButton.Text
             #endregion
+        }
+
+        internal static void TaxCalCreate(TextBox calBox, TextBox formulaBox, TextBox allCalBox, Button taxButton, Button noTaxButton, object sender)
+        {
+            if (sender.Equals(taxButton))
+            {
+                if (calBox.Text == "")
+                {
+                    return;
+                }
+
+                string a = calBox.Text;
+
+                string tax = a + "* 1.1";
+
+                DataTable dt = new DataTable();
+                var result = dt.Compute(tax, "");
+
+                calBox.Text = Math.Floor((decimal)result).ToString();
+                formulaBox.Text = $"{a}円 ⇒ 税込 : " + Math.Floor((decimal)result) + "円";
+                allCalBox.Text += formulaBox.Text + "\r\n";
+            }
+            else if (sender.Equals(noTaxButton))
+            {
+                if (calBox.Text == "")
+                {
+                    return;
+                }
+                string a = calBox.Text;
+
+                string tax = a + "/ 1.1 + 0.1";
+
+                DataTable dt = new DataTable();
+                var result = dt.Compute(tax, "");
+
+                calBox.Text = Math.Floor((decimal)result).ToString();
+                formulaBox.Text = $"{a}円 ⇒ 税抜 : " + Math.Floor((decimal)result) + "円";
+                allCalBox.Text += formulaBox.Text + "\r\n";
+            }
         }
     }
 }
